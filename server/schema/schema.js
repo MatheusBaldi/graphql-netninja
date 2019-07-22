894b18cce99c099cefd3ef1de1e5a434953fc520
Matheus Baldi <matheushbaldi@gmail.com>
2019-07-22 18:02:07 -0300

    nested author to books and nested books to authors added; authors and books queries added

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,13 +7,17 @@ const {
     GraphQLString, 
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 var books = [
     {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
     {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
     {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+    {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+    {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+    {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'},
 ];
 
 var authors = [
@@ -32,7 +36,6 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve(parent, args){
-                console.log(parent);
                 return _.find(authors, {id:parent.authorId});
             }
         }
@@ -44,7 +47,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        books:{
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return _.filter(books, {authorId: parent.id});
+            }
+        }
     })
 });
 
@@ -64,10 +73,22 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args){
                 return _.find(authors, {id: args.id});
             }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return books;
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args){
+                return authors;
+            }
         }
     }
 });
 
 module.exports = new GraphQLSchema({
     query: RootQuery
-})
+});
