f589d06b1b26ad21b6484ca96a8d7a325365bcc8
Matheus Baldi <matheushbaldi@gmail.com>
2019-07-22 22:59:27 -0300

    Added mongoose connection; added book and author models

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+mongoose.connection.once('open', ()=>{
+    console.log('Connected to DB');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
