3347939f5773eba46e8e0d2ce429cfb4c7406c4e
Matheus Baldi <matheushbaldi@gmail.com>
2019-07-24 18:36:48 -0300

    Added react frontend with graphql integration using apollo; Fixed error in books query

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// Components
+import BookList from './components/BookList';
+
+// Apolo cliente setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ninja's Reading List</h1>
+        <BookList/>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// Allow cross-origin requests
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 mongoose.connection.once('open', ()=>{
